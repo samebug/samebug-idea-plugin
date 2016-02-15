9ace26b9aac0a2a083b7c3f1fa9a8423012f2f6b
Poroszkai Daniel <poroszd@inf.elte.hu>
2016-02-15 14:32:16 +0100

    format, license

--- src/main/java/com/samebug/clients/idea/components/application/IdeaSamebugPlugin.java
+++ src/main/java/com/samebug/clients/idea/components/application/IdeaSamebugPlugin.java
@@ -15,16 +15,12 @@
  */
 package com.samebug.clients.idea.components.application;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
-import com.samebug.clients.idea.notification.OperationalNotification;
 import com.samebug.clients.idea.notification.SamebugNotifications;
-import com.samebug.clients.idea.notification.SearchResultsNotification;
 import com.samebug.clients.idea.ui.SettingsDialog;
 import com.samebug.clients.search.api.SamebugClient;
 import com.samebug.clients.search.api.entities.UserInfo;
--- src/main/java/com/samebug/clients/idea/components/project/UtilComponent.java
+++ src/main/java/com/samebug/clients/idea/components/project/UtilComponent.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Samebug, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.samebug.clients.idea.components.project;
 
 import com.intellij.notification.Notification;
@@ -10,11 +25,11 @@ import com.samebug.clients.idea.resources.SamebugBundle;
 
 /**
  * Created by poroszd on 2/15/16.
- *
+ * <p/>
  * I found no reasonable place for the one-time welcome message.
  * IdeaSamebugPlugin would be a better place, but I wanted to make sure there is an opened
  * project in scope, so clicking on the notification can open the samebug toolbar.
- *
+ * <p/>
  * Renaming this class or moving this functionality is welcomed.
  */
 public class UtilComponent extends AbstractProjectComponent {
