b706c657cd546de6a255c9b4081de914bc5c3275
Poroszkai Daniel <poroszd@inf.elte.hu>
2016-03-23 16:25:04 +0100

    component breadcrumb force in one line

--- src/main/java/com/samebug/clients/idea/ui/components/BreadcrumbBar.java
+++ src/main/java/com/samebug/clients/idea/ui/components/BreadcrumbBar.java
@@ -16,7 +16,7 @@ import java.awt.event.MouseEvent;
 public class BreadcrumbBar extends JPanel {
     public BreadcrumbBar(java.util.List<ComponentStack> stacks) {
         assert (stacks.size() > 0);
-        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
         setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
         add(new BreadcrumbEndLabel());
         add(new BreadcrumbLabel(stacks.get(0)));
