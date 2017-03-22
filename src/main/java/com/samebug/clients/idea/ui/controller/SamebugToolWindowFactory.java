c3ac6158beeda434142bcdc5f86a85e2f2061c0d
Poroszkai Daniel <poroszd@inf.elte.hu>
2017-03-22 11:20:30 +0100

    starting toolwindow in non-docked mode

--- src/main/java/com/samebug/clients/idea/ui/controller/SamebugToolWindowFactory.java
+++ src/main/java/com/samebug/clients/idea/ui/controller/SamebugToolWindowFactory.java
@@ -19,9 +19,15 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.openapi.wm.ToolWindowType;
+import com.intellij.openapi.wm.impl.ToolWindowImpl;
+import com.intellij.openapi.wm.impl.ToolWindowManagerImpl;
+import com.intellij.openapi.wm.impl.WindowInfoImpl;
 import com.samebug.clients.idea.components.project.SamebugProjectComponent;
 import com.samebug.clients.idea.ui.controller.toolwindow.ToolWindowController;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Method;
 
 
 final public class SamebugToolWindowFactory implements ToolWindowFactory, DumbAware {
@@ -30,5 +36,30 @@ final public class SamebugToolWindowFactory implements ToolWindowFactory, DumbAw
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ToolWindowController twc = project.getComponent(SamebugProjectComponent.class).getToolWindowController();
         twc.initToolWindow(toolWindow);
+
+        // do not start in docked mode
+        toolWindow.setDefaultState(null, ToolWindowType.SLIDING, null);
+
+        // there is no public interface to change the default autohide property, so we use reflection and don't care if it fails
+        try {
+            ToolWindowManagerImpl twm = ((ToolWindowImpl) toolWindow).getToolWindowManager();
+            Method twmGetInfo = twm.getClass().getDeclaredMethod("getInfo", String.class);
+            twmGetInfo.setAccessible(true);
+            WindowInfoImpl windowInfo = (WindowInfoImpl) twmGetInfo.invoke(twm, "Samebug");
+            Method windowInfoWasRead = windowInfo.getClass().getDeclaredMethod("wasRead");
+            windowInfoWasRead.setAccessible(true);
+            boolean wasRead = (Boolean) windowInfoWasRead.invoke(windowInfo);
+            if (!wasRead) {
+                toolWindow.setAutoHide(true);
+            }
+        } catch (Exception ignored) {
+        }
+
+        // TODO for now, we also force the behaviour of the toolwindow, which will override the settings even if the user has changed it. Remove it asap.
+        try {
+            toolWindow.setType(ToolWindowType.SLIDING, null);
+            toolWindow.setAutoHide(true);
+        } catch (Exception ignored) {
+        }
     }
 }
