3c9a7c4981dfde3d5f78b78d77dd3438c08c9608
Poroszkai Daniel <poroszd@inf.elte.hu>
2017-03-27 14:12:03 +0200

    simplify tab rect computation code, make sure that the selected tab does not get moved

--- src/main/java/com/samebug/clients/swing/ui/base/tabbedPane/SamebugTabbedPaneUI.java
+++ src/main/java/com/samebug/clients/swing/ui/base/tabbedPane/SamebugTabbedPaneUI.java
@@ -83,48 +83,26 @@ public class SamebugTabbedPaneUI extends BasicTabbedPaneUI {
             @Override
             protected void calculateTabRects(int tabPlacement, int tabCount) {
                 FontMetrics metrics = getFontMetrics();
-                // This method is almost exact copy of the super, but uses a fictional large size instead of the tabPane's real size
-//                Dimension size = tabPane.getSize();
-                Dimension size = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
                 Insets insets = tabPane.getInsets();
                 Insets tabAreaInsets = getTabAreaInsets(tabPlacement);
-                int fontHeight = metrics.getHeight();
                 int selectedIndex = tabPane.getSelectedIndex();
                 int tabRunOverlay;
                 int i, j;
                 int x, y;
-                int returnAt;
-                boolean verticalTabRuns = (tabPlacement == LEFT || tabPlacement == RIGHT);
-                boolean leftToRight = true;
 
                 //
                 // Calculate bounds within which a tab run must fit
                 //
                 switch (tabPlacement) {
                     case LEFT:
-                        maxTabWidth = calculateMaxTabWidth(tabPlacement);
-                        x = insets.left + tabAreaInsets.left;
-                        y = insets.top + tabAreaInsets.top;
-                        returnAt = size.height - (insets.bottom + tabAreaInsets.bottom);
-                        break;
                     case RIGHT:
-                        maxTabWidth = calculateMaxTabWidth(tabPlacement);
-                        x = size.width - insets.right - tabAreaInsets.right - maxTabWidth;
-                        y = insets.top + tabAreaInsets.top;
-                        returnAt = size.height - (insets.bottom + tabAreaInsets.bottom);
-                        break;
                     case BOTTOM:
-                        maxTabHeight = calculateMaxTabHeight(tabPlacement);
-                        x = insets.left + tabAreaInsets.left;
-                        y = size.height - insets.bottom - tabAreaInsets.bottom - maxTabHeight;
-                        returnAt = size.width - (insets.right + tabAreaInsets.right);
-                        break;
+                        throw new UnsupportedOperationException();
                     case TOP:
                     default:
                         maxTabHeight = calculateMaxTabHeight(tabPlacement);
                         x = insets.left + tabAreaInsets.left;
                         y = insets.top + tabAreaInsets.top;
-                        returnAt = size.width - (insets.right + tabAreaInsets.right);
                         break;
                 }
 
@@ -142,77 +120,23 @@ public class SamebugTabbedPaneUI extends BasicTabbedPaneUI {
                 for (i = 0; i < tabCount; i++) {
                     rect = rects[i];
 
-                    if (!verticalTabRuns) {
-                        // Tabs on TOP or BOTTOM....
-                        if (i > 0) {
-                            rect.x = rects[i - 1].x + rects[i - 1].width;
-                        } else {
-                            tabRuns[0] = 0;
-                            runCount = 1;
-                            maxTabWidth = 0;
-                            rect.x = x;
-                        }
-                        rect.width = calculateTabWidth(tabPlacement, i, metrics);
-                        maxTabWidth = Math.max(maxTabWidth, rect.width);
-
-                        // Never move a TAB down a run if it is in the first column.
-                        // Even if there isn't enough room, moving it to a fresh
-                        // line won't help.
-                        if (rect.x != x && rect.x + rect.width > returnAt) {
-                            if (runCount > tabRuns.length - 1) {
-                                expandTabRunsArray();
-                            }
-                            tabRuns[runCount] = i;
-                            runCount++;
-                            rect.x = x;
-                        }
-                        // Initialize y position in case there's just one run
-                        rect.y = y;
-                        rect.height = maxTabHeight/* - 2*/;
-
+                    // Tabs on TOP or BOTTOM....
+                    if (i > 0) {
+                        rect.x = rects[i - 1].x + rects[i - 1].width;
                     } else {
-                        // Tabs on LEFT or RIGHT...
-                        if (i > 0) {
-                            rect.y = rects[i - 1].y + rects[i - 1].height;
-                        } else {
-                            tabRuns[0] = 0;
-                            runCount = 1;
-                            maxTabHeight = 0;
-                            rect.y = y;
-                        }
-                        rect.height = calculateTabHeight(tabPlacement, i, fontHeight);
-                        maxTabHeight = Math.max(maxTabHeight, rect.height);
-
-                        // Never move a TAB over a run if it is in the first run.
-                        // Even if there isn't enough room, moving it to a fresh
-                        // column won't help.
-                        if (rect.y != y && rect.y + rect.height > returnAt) {
-                            if (runCount > tabRuns.length - 1) {
-                                expandTabRunsArray();
-                            }
-                            tabRuns[runCount] = i;
-                            runCount++;
-                            rect.y = y;
-                        }
-                        // Initialize x position in case there's just one column
+                        tabRuns[0] = 0;
+                        runCount = 1;
+                        maxTabWidth = 0;
                         rect.x = x;
-                        rect.width = maxTabWidth/* - 2*/;
-
                     }
+                    rect.width = calculateTabWidth(tabPlacement, i, metrics);
+                    maxTabWidth = Math.max(maxTabWidth, rect.width);
+
+                    // Initialize y position in case there's just one run
+                    rect.y = y;
+                    rect.height = maxTabHeight/* - 2*/;
                     if (i == selectedIndex) {
                         selectedRun = runCount - 1;
-                    }
-                }
-
-                if (runCount > 1) {
-                    // Re-distribute tabs in case last run has leftover space
-                    normalizeTabRuns(tabPlacement, tabCount, verticalTabRuns ? y : x, returnAt);
-
-                    selectedRun = getRunForTab(tabCount, selectedIndex);
-
-                    // Rotate run array so that selected run is first
-                    if (shouldRotateTabRuns(tabPlacement)) {
-                        rotateTabRuns(tabPlacement, selectedRun);
                     }
                 }
 
@@ -222,48 +146,12 @@ public class SamebugTabbedPaneUI extends BasicTabbedPaneUI {
                     int start = tabRuns[i];
                     int next = tabRuns[i == (runCount - 1) ? 0 : i + 1];
                     int end = (next != 0 ? next - 1 : tabCount - 1);
-                    if (!verticalTabRuns) {
-                        for (j = start; j <= end; j++) {
-                            rect = rects[j];
-                            rect.y = y;
-                            rect.x += getTabRunIndent(tabPlacement, i);
-                        }
-                        if (shouldPadTabRun(tabPlacement, i)) {
-                            padTabRun(tabPlacement, start, end, returnAt);
-                        }
-                        if (tabPlacement == BOTTOM) {
-                            y -= (maxTabHeight - tabRunOverlay);
-                        } else {
-                            y += (maxTabHeight - tabRunOverlay);
-                        }
-                    } else {
-                        for (j = start; j <= end; j++) {
-                            rect = rects[j];
-                            rect.x = x;
-                            rect.y += getTabRunIndent(tabPlacement, i);
-                        }
-                        if (shouldPadTabRun(tabPlacement, i)) {
-                            padTabRun(tabPlacement, start, end, returnAt);
-                        }
-                        if (tabPlacement == RIGHT) {
-                            x -= (maxTabWidth - tabRunOverlay);
-                        } else {
-                            x += (maxTabWidth - tabRunOverlay);
-                        }
+                    for (j = start; j <= end; j++) {
+                        rect = rects[j];
+                        rect.y = y;
+                        rect.x += getTabRunIndent(tabPlacement, i);
                     }
-                }
-
-                // Pad the selected tab so that it appears raised in front
-                padSelectedTab(tabPlacement, selectedIndex);
-
-                // if right to left and tab placement on the top or
-                // the bottom, flip x positions and adjust by widths
-                if (!leftToRight && !verticalTabRuns) {
-                    int rightMargin = size.width
-                            - (insets.right + tabAreaInsets.right);
-                    for (i = 0; i < tabCount; i++) {
-                        rects[i].x = rightMargin - rects[i].x - rects[i].width;
-                    }
+                    y += (maxTabHeight - tabRunOverlay);
                 }
             }
         };
