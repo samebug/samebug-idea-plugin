dd93bc0a158bbf4168d4846acea77db5797f5434
Poroszkai Daniel <poroszd@inf.elte.hu>
2017-04-25 16:22:33 +0200

    exclude ui and http tests

--- src/test/java/com/samebug/clients/swing/ui/component/popup/HelpRequestPopupTest.java
+++ src/test/java/com/samebug/clients/swing/ui/component/popup/HelpRequestPopupTest.java
@@ -2,12 +2,10 @@ package com.samebug.clients.swing.ui.component.popup;
 
 import com.samebug.clients.common.ui.component.popup.IHelpRequestPopup;
 import com.samebug.clients.swing.ui.TestDialog;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.awt.*;
 
-@Ignore
 public class HelpRequestPopupTest extends TestDialog {
     @Test
     public void popup() {
