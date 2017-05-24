54333cc5b764477d6decf2b906c6ed028c3c1555
Poroszkai Daniel <daniel.poroszkai@samebug.io>
2017-05-24 15:37:32 +0200

    fix notification type

--- src/main/java/com/samebug/clients/http/json/NotificationAdapter.java
+++ src/main/java/com/samebug/clients/http/json/NotificationAdapter.java
@@ -23,7 +23,7 @@ import com.samebug.clients.http.entities.notification.Notification;
 public class NotificationAdapter extends AbstractObjectAdapter<Notification> {
     {
         typeClasses = ImmutableMap.<String, Class<? extends Notification>>builder()
-                .put("incoming--help-request", IncomingHelpRequest.class)
+                .put("incoming--help-request--created", IncomingHelpRequest.class)
                 .put("incoming--answer", IncomingAnswer.class)
                 .build();
     }
